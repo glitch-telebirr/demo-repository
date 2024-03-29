1688290c01c5e187fd06cf1840df5491a0814561
eyuel-msiew <165380946+eyuel-msiew@users.noreply.github.com>
2024-03-29 21:43:46 +0300

    updated
    Signed-off-by: eyuel-msiew <165380946+eyuel-msiew@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BXX9NDALB3l27ANBGm7HTFzWR8yYSko",
-  authDomain: "telebirr-playstore.firebaseapp.com",
-  projectId: "telebirr-playstore",
-  storageBucket: "telebirr-playstore.appspot.com",
-  messagingSenderId: "537049986268",
-  appId: "1:537049986268:android:e36cafaa5afc54419f3019",
-  measurementId: "G-TPYDRSZ922", // Omit if not using Google Analytics
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID", // Omit if not using Google Analytics
 };
 export { firebaseConfig };
