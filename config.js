16e9170b9b82e0c98be3df56239731f2e04238f8
eyuel-msiew <165380946+eyuel-msiew@users.noreply.github.com>
2024-03-29 21:41:33 +0300

    Update
    Signed-off-by: eyuel-msiew <165380946+eyuel-msiew@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA9BXX9NDALB3l27ANBGm7HTFzWR8yYSko",
+  authDomain: "telebirr-playstore.firebaseapp.com",
+  projectId: "telebirr-playstore",
+  storageBucket: "telebirr-playstore.appspot.com",
+  messagingSenderId: "537049986268",
+  appId: "1:537049986268:android:e36cafaa5afc54419f3019",
+  measurementId: "G-TPYDRSZ922", // Omit if not using Google Analytics
+};
+export { firebaseConfig };
