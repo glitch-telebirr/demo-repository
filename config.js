2daf1cc3280e856a50be9589b2594aa3c535df46
eyuel-msiew <165380946+eyuel-msiew@users.noreply.github.com>
2024-03-31 15:08:24 +0300

    Update
    Signed-off-by: eyuel-msiew <165380946+eyuel-msiew@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID", // Omit if not using Google Analytics
+  apiKey: "AIzaSyDpPbpRizhxZVZzWEGYU2_j393gomy_lao",
+  authDomain: "telebirr-playstore.firebaseapp.com",
+  databaseURL: "https://telebirr-playstore-default-rtdb.firebaseio.com",
+  projectId: "telebirr-playstore",
+  storageBucket: "telebirr-playstore.appspot.com",
+  messagingSenderId: "537049986268",
+  appId: "1:537049986268:web:d5f5e5f7434bcf599f3019",
+  measurementId: "G-L1QT9EL642"
 };
-export { firebaseConfig };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
